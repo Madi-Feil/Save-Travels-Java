67df236c9e46fbdcb89a284fc4b4394f7ed3ebaf
Madi Feil <98246671+Madi-Feil@users.noreply.github.com>
2022-05-29 22:12:31 -0700

    Add files via upload

---
+++ SaveTravelsCoreApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.savetravels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveTravelsCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveTravelsCoreApplication.class, args);
+	}
+
+}
