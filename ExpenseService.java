281c1be15db5045656111580f2af1d0b6b3bed85
Madi Feil <98246671+Madi-Feil@users.noreply.github.com>
2022-05-29 22:12:43 -0700

    Add files via upload

---
+++ ExpenseService.java
@@ -0,0 +1,39 @@
+package com.codingdojo.savetravels.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.codingdojo.savetravels.models.Expense;
+import com.codingdojo.savetravels.repositories.ExpenseRepository;
+
+
+
+@Service
+public class ExpenseService {
+	private final ExpenseRepository repo;
+	public ExpenseService(ExpenseRepository repo) {
+		this.repo = repo;
+	}
+	public List<Expense> all() {
+		return repo.findAll();
+	}
+	public Expense create(Expense b) {
+		return repo.save(b);
+	}
+	public Expense update(Expense b) {
+		return repo.save(b);
+	}
+	public Expense find(Long id) {
+		Optional<Expense> optional = repo.findById(id);
+		if(optional.isPresent()) {
+			return optional.get();
+		}else {
+			return null;
+		}
+	}
+	public void delete(Long id) {
+		repo.deleteById(id);
+	}
+}
